7cb74dc9b7ccafa303adaeac6e11a08afc9b0968
Zhang <2297428445@qq.com>
2021-06-12 21:50:46 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/coolweather/android/gson/Basic.java
@@ -0,0 +1,17 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Basic {
+    @SerializedName("city")
+    public String cityName;
+
+    @SerializedName("id")
+    public String weatherId;
+
+    public Updata updata;
+    public class Updata{
+        @SerializedName("loc")
+        public String updataTime;
+    }
+}
